021b42ef99c3e28e9eb1beaff0ddbe00a74536a8
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-05 11:27:24 +0000

    some changes - working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import './App.css';
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState('');
 
   useEffect(() => {
     fetch('http://localhost:3000/')
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => setMessage(data.message));
   });
 
   return (
-    <h1>Intro to Express</h1>
+    <p>Message from server: {message}</p>
   );
 }
 
