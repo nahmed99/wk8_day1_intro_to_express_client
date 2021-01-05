c943e089fa9535bb8e9f242993ed380f935a5735
Nawaz Ahmed <ahmed_nawaz@hotmail.com>
2021-01-05 11:10:26 +0000

    useEffect added to client app.cs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import React, {useEffect} from 'react';
 
 function App() {
+
+  useEffect(() => {
+    fetch('http://localhost:3000/')
+    .then(response => response.json())
+    .then(data => console.log(data));
+  });
+
   return (
     <h1>Intro to Express</h1>
   );
